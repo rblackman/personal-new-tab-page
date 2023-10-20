691d26af06835eaa55d392d55234d424472807bf
Ryan Blackman <rblackman@mtd.org>
2023-10-20 12:51:59 -0500

    feature: format lint

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,18 +1,17 @@
 import path from 'path';
 
 const buildEslintCommand = (filenames) => [
-	`eslint --fix  ${filenames.map((f) => path.relative(process.cwd(), f)).join('  ')}`,
+	`eslint --fix  ${filenames.map((f) => path.relative(process.cwd(), f)).join('  ')}`
 ];
 
 const buildPrettierCommand = (filenames) => [
-	`prettier ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')} --write`,
+	`prettier ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')} --write`
 ];
-
 
 const config = {
 	'**/*.(ts|tsx)': () => 'npx tsc --noEmit',
 	'*.{js,jsx,ts,tsx}': [buildPrettierCommand, buildEslintCommand],
-	'**/*.(md|json)': [buildPrettierCommand],
+	'**/*.(md|json)': [buildPrettierCommand]
 };
 
 export default config;
