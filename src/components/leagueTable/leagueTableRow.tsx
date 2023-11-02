ebce2016e5c44e867134402a4cca8262dc3ea6d8
Ryan Blackman <rblackman@mtd.org>
2023-11-02 10:47:08 -0500

    feature: add league table modifier

--- src/components/leagueTable/leagueTableRow.tsx
+++ src/components/leagueTable/leagueTableRow.tsx
@@ -12,11 +12,20 @@ interface Props extends Standing {
 
 export default function LeagueTableRow({ team, stats, small, form, noHighlight }: Props) {
 	const { id, shortName, logo } = team;
-	const { currentPosition, goalDifference, goalsFor, goalsAgainst, points, gamesPlayed, win, draw, loss, form: leagueForm } = stats;
+	const { currentPosition, goalDifference, goalsFor, goalsAgainst, points, gamesPlayed, win, draw, loss, form: leagueForm, standingModifier } = stats;
+
+	const modifierClasses = clsx({
+		[styles.smr]: true,
+		[styles.modifier]: true,
+		[styles.cl]: standingModifier === "Champion's League",
+		[styles.el]: standingModifier === 'Europa League',
+		[styles.ecl]: standingModifier === 'Europa Conference League',
+		[styles.rel]: standingModifier === 'Relegation'
+	});
 
 	return (
 		<tr className={clsx({ [styles.highlight]: highlightTeam === id && !noHighlight })}>
-			<td className={styles.smr}>{currentPosition}</td>
+			<td className={modifierClasses}>{currentPosition}</td>
 			<td className={styles.lg}>
 				<span className={styles.team}>
 					<img className={styles.img} src={logo} alt={`${name} logo`} />
