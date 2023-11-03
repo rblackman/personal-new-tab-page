edbf935ae38a2cea8f592c56e72d63bce4d94377
Ryan Blackman <rblackman@mtd.org>
2023-11-03 11:05:46 -0500

    bug: fix auto versioing hooks

--- bump-version.mjs
+++ bump-version.mjs
@@ -4,9 +4,17 @@ import path from 'node:path';
 const dirname = path.dirname('.');
 const file = await fs.readFile(path.join(dirname, 'manifest.json'), 'utf-8');
 const manifest = JSON.parse(file);
+
+console.debug('read manifest.json');
+
 const packageFile = await fs.readFile(path.join(dirname, 'package.json'), 'utf-8');
 const packageJson = JSON.parse(packageFile);
-const version = packageJson.version;
-manifest.version = version;
+
+console.debug('read package.json');
+
+manifest.version = packageJson.version;
+console.info('updated manifest.json to version', packageJson.version);
 
 await fs.writeFile(path.join(dirname, 'manifest.json'), JSON.stringify(manifest, null, '\t'));
+
+console.debug('wrote manifest.json');
