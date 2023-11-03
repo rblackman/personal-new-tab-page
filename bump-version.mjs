1bc2b9fc6e0b21a0f0896dc226123905b2e2fe16
Ryan Blackman <rblackman@mtd.org>
2023-11-03 11:00:53 -0500

    feature: fix auto versioning

--- bump-version.mjs
+++ bump-version.mjs
@@ -4,10 +4,9 @@ import path from 'node:path';
 const dirname = path.dirname('.');
 const file = await fs.readFile(path.join(dirname, 'manifest.json'), 'utf-8');
 const manifest = JSON.parse(file);
-const version = manifest.version;
-const versions = version.split('.');
-const patch = parseInt(versions[2], 10) + 1;
-const newVersion = `${versions[0]}.${versions[1]}.${patch}`;
-manifest.version = newVersion;
+const packageFile = await fs.readFile(path.join(dirname, 'package.json'), 'utf-8');
+const packageJson = JSON.parse(packageFile);
+const version = packageJson.version;
+manifest.version = version;
 
 await fs.writeFile(path.join(dirname, 'manifest.json'), JSON.stringify(manifest, null, '\t'));
